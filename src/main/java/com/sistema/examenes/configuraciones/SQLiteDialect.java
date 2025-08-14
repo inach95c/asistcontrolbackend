fda29b8cfe29c52aa15712b07522f43417107272
Inach <inach95c@gmail.com>
2025-08-14 16:16:25 -0400

    Primera versión lista para despliegue

---
+++ src/main/java/com/sistema/examenes/configuraciones/SQLiteDialect.java
@@ -0,0 +1,57 @@
+package com.sistema.examenes.configuraciones;
+
+import java.sql.Types;
+
+import org.hibernate.dialect.Dialect;
+import org.hibernate.dialect.function.StandardSQLFunction;
+import org.hibernate.dialect.function.SQLFunctionTemplate;
+import org.hibernate.dialect.function.VarArgsSQLFunction;
+import org.hibernate.dialect.identity.IdentityColumnSupport;
+import org.hibernate.dialect.identity.IdentityColumnSupportImpl;
+import org.hibernate.type.StringType;
+
+public class SQLiteDialect extends Dialect {
+
+    public SQLiteDialect() {
+        super();
+        registerColumnType(Types.INTEGER, "integer");
+        registerColumnType(Types.VARCHAR, "text");
+        registerColumnType(Types.BLOB, "blob");
+        registerColumnType(Types.REAL, "real");
+        registerColumnType(Types.DOUBLE, "double");
+        registerColumnType(Types.FLOAT, "float");
+        registerColumnType(Types.BOOLEAN, "boolean");
+
+        registerFunction("concat", new VarArgsSQLFunction(StringType.INSTANCE, "", "||", ""));
+        registerFunction("mod", new SQLFunctionTemplate(StringType.INSTANCE, "?1 % ?2"));
+        registerFunction("substr", new StandardSQLFunction("substr", StringType.INSTANCE));
+        registerFunction("substring", new StandardSQLFunction("substr", StringType.INSTANCE));
+    }
+
+    @Override
+    public IdentityColumnSupport getIdentityColumnSupport() {
+        return new SQLiteIdentityColumnSupport();
+    }
+
+    public boolean supportsIdentityColumns() {
+        return true;
+    }
+
+    public boolean hasDataTypeInIdentityColumn() {
+        return false;
+    }
+
+    public String getIdentityColumnString() {
+        return "integer";
+    }
+
+   
+    public String getIdentitySelectString() {
+        return "select last_insert_rowid()";
+    }
+    
+    @Override
+    public String getAddColumnString() {
+        return "add column";
+    }
+}
