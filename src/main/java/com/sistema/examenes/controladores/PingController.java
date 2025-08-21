adff0e7195c508ef26e6779d8b2a926e7a78566a
santodaniel <santodaniel@nauta.cu>
2025-08-21 13:40:59 -0400

    con pin del frontend

---
+++ src/main/java/com/sistema/examenes/controladores/PingController.java
@@ -0,0 +1,13 @@
+package com.sistema.examenes.controladores;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PingController {
+    @GetMapping("/ping")
+    public ResponseEntity<String> ping() {
+        return ResponseEntity.ok("pong");
+    }
+}
